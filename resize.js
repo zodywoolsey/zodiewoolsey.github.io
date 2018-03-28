88b8f8766e6bb72bf98b12c116b918c0ed555300
Zody <zodywoolsey@users.noreply.github.com>
2018-03-27 21:31:12 -0500

    update

--- resize.js
+++ resize.js
@@ -1,12 +1,12 @@
 function HvW(){
     if(screen.availHeight > screen.availWidth){
         document.querySelector("ul").style.fontSize = "2em";
-        document.getElementById("foot").style.fontSize = "2em";
+        document.getElementById("foot").style.fontSize = "1.5em";
         document.querySelector("h1").style.fontSize = "3em";
         document.getElementById("paras").style.fontSize = "1em";
     }else if(screen.availHeight < screen.availWidth){
         document.querySelector("ul").style.fontSize = "2em";
-        document.getElementById("foot").style.fontSize = "2em";
+        document.getElementById("foot").style.fontSize = "1.5em";
         document.querySelector("h1").style.fontSize = "3em";
         document.getElementById("paras").style.fontSize = "1em";
     }
