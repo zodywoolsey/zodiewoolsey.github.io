81337b898b0246dc1f4911d17d1bdffda4c2e6a0
Zody <zodywoolsey@gmail.com>
2018-04-29 21:13:15 -0500

    misc

--- EXPAND.js
+++
@@ -1,7 +0,0 @@
-if(sessionStorage.getItem("visited") != "1"){
-    document.getElementsByClassName("html")[0].classList.add("init");
-}
-sessionStorage.setItem("visited", "1");
-setTimeout(function(){
-    document.getElementsByClassName("html")[0].classList.remove("init");
-}, 1);
