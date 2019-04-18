54992d16456dd5763f7c60cbceba40cf5f92a4d3
zodywoolsey <zodywoolsey@gmail.com>
2019-04-18 18:22:54 -0500

    Moved and streamlined pageSwaps.js Created first obj in projParser.js

--- pageSwaps.js
+++ pageSwaps.js
@@ -1,4 +1,18 @@
-/* PROJECT OBJECT */
-let proj = {
-    //Research how to make data structures
+function toPage(url){
+    let transitionDuration = window.getComputedStyle(pageContainer).transitionDuration.replace(/\D/, '' )*1000;
+    toPageStart();
+    let pageGetter = new XMLHttpRequest();
+    pageGetter.open('GET',url, true);
+    pageGetter.onreadystatechange = ()=>{
+        if(pageGetter.readyState === 4 && pageGetter.status === 200){
+            let newPage = document.createElement('body');
+            newPage.innerHTML = pageGetter.response;
+            // console.log(pageGetter.response);
+            document.querySelectorAll('body')[0].replaceWith(newPage);
+            toPageEnd();
+        }
+    }
+    setTimeout( ()=>{
+        pageGetter.send();
+    }, 1000 )
 }
