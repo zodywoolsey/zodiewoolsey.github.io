4334b59076475cd7b543f185675996e028cee06b
zodywoolsey <zodywoolsey@gmail.com>
2019-04-18 11:08:00 -0500

    removed extra lines and functions

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-let portfolioBody = document.querySelectorAll('body')[0];
-
-let resumeGetter = new XMLHttpRequest()
-resumeGetter.open('GET', 'resume.html', false);
-resumeGetter.send();
-
-let resumePageContainer = document.createElement('body');
-resumePageContainer.innerHTML = resumeGetter.response;
-
-function toResumePage(){
-    portfolioBody.replaceWith(resumePageContainer);
-}
-function toPortfolioPage(){
-    resumePageContainer.replaceWith(portfolioBody);
-}
-function toGoogle(){
-    let gpageGetter = new XMLHttpRequest();
-    gpageGetter.open('GET', 'www.google.com', false);
-    gpageGetter.send();
-    let gpage = document.createElement('body');
-    gpage.innerHTML = gpageGetter.response;
-    document.all[0].replaceWith();
-}
-
 function toPage(url){
     let transitionDuration = window.getComputedStyle(pageContainer).transitionDuration.replace(/\D/, '' )*1000;
     toPageStart();
@@ -31,7 +7,7 @@ function toPage(url){
         if(pageGetter.readyState === 4 && pageGetter.status === 200){
             let newPage = document.createElement('body');
             newPage.innerHTML = pageGetter.response;
-            console.log(pageGetter.response);
+            // console.log(pageGetter.response);
             document.querySelectorAll('body')[0].replaceWith(newPage);
             toPageEnd();
         }
