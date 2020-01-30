3b52cc0dacee4a7f99b1b88528e3ec3668410f90
zodywoolsey <zodywoolsey@gmail.com>
2020-01-29 20:11:37 -0600

    Uploaded pwa test

---
+++ pwa/serviceworker.js
@@ -0,0 +1,39 @@
+self.importScripts('main.js')
+
+var cacheName = 'calendarTest'
+var appShellFiles = [
+    `/`,
+    `/img/`,
+    `/img/test.jpg`,
+    `/icon/`,
+    `/icon/icon.png`,
+    `/app.js`,
+    `/main.js`,
+    `/index.html`,
+    `/style.css`
+]
+
+self.addEventListener('install', function(e){
+    console.log('[Service Worker] Install')
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache){
+            console.log('[Service Worker] Caching all: app shell and content')
+            return cache.addAll(contentToCache);
+        })
+    )
+})
+
+self.addEventListener('fetch',function(e){
+    e.responWith(
+        caches.match(e.request).then(function(r){
+            console.log(`[Service Worker] Fetching resource: ${e.request.url}` )
+            return r || fetch(e.request).then(function(response){
+                return caches.open(cacheName).then(function(cache){
+                    console.log(`[service Worker] Caching new resource: ${e.request.url}`)
+                    cache.put(e.request, response.clone())
+                    return response
+                })
+            })
+        })
+    )
+})
