80411bb8ed2930bd26455527d4671841f2e9a727
zodiepupper <zodywoolsey@users.noreply.github.com>
2022-11-15 13:45:44 -0700

    react

--- old/xrscripts/aframe.js
+++
@@ -1,8 +0,0 @@
-AFRAME.registerComponent('collider-check', {
-    dependencies: ['raycaster'],
-    init: function(){
-    this.el.addEventListener('raycaster-intersection', function(){
-        console.log('hit planet');
-    });
-    }
-});
