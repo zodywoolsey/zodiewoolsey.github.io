42eafb3d8469d251658415eae8d3874d08db0fc7
zodiepupper <zodywoolsey@users.noreply.github.com>
2022-11-15 17:57:19 -0700

    new site!

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className='bg-neutral'>
+    <div className="navbar bg-primary">
+      <div className="navbar-start">
+      </div>
+      <div className="navbar-center">
+        <a className="btn btn-ghost normal-case text-xl">zodie@pupper.dev</a>
+      </div>
+      <div className="navbar-end">
+      </div>
+    </div>
+  <Component {...pageProps} />
+  </div>
+  )
 }
 
 export default MyApp
