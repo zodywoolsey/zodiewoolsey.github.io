58f75a8d9b7494339ef93c1cba057b58fd28063f
zodiepupper <zodywoolsey@users.noreply.github.com>
2023-06-09 00:13:32 -0600

    upd - added links for bark, socials, and support

--- pages/index.js
+++ pages/index.js
@@ -12,15 +12,56 @@ export default function Home() {
         <div className="card-body">
           <h2 className="card-title text-primary-content text-4xl">Hi, I'm Zodie</h2>
           <div>
-            <p className='text-primary-content text-lg m-4' >I've been writing code since 2011.</p>
-            <p className='text-primary-content text-lg m-4' >I taught myself python in 7th grade so I could automate my math homework and have been in love ever since.</p>
-            <p className='text-primary-content text-lg m-4' >My favorite things to create are games and physics simulations, but I've done all kinds of things.</p>
-            <p className='text-primary-content text-lg m-4 hidden xs:block' >My experience includes building websites, basic ocr, XR, gameplay, shaders, design, marketing, video/photo editing, etc. If you need something and it can be done on a computer, I can probably figure it out.</p>
+            <p className='text-primary-content text-lg m-4' >I'm working on a better future for social XR applications</p>
+            <p className='text-primary-content text-lg m-4' >My current project is called "bark" and our goal is to create an open source social XR ecosystem to provide a better ground layer for future platforms</p>
+            <p className='text-primary-content text-lg m-4' >We are currently developing our client and will be working to publish a spec soon</p>
+            <p className='text-primary-content text-lg m-4 hidden xs:block' >We plan to federate with every other platform we can and offer an open set of tools and ideas to push for a better future</p>
           </div>
         </div>
       </div>
       
     </div>
+      <div className='flex justify-evenly'>
+        {/* SUPPORT */}
+        <div className="card w-full lg:w-2/3 h-80 bg-base-100 text-primary-content m-2">
+          <div className="card-body">
+            <img className='max-w-[4rem] max-h-16 absolute top-0 right-0 m-8 hidden md:block' src={"/doge-color-new.svg"}/>
+            <h2 className="card-title text-4xl">Please consider supporting us</h2>
+            <p>The following links are for support vectors to support the team
+              <br/>
+              and socials for contact or joining the community
+            </p>
+            <div className="card-actions justify-end ">
+              <Link href={"https://www.patreon.com/pupperdev"} className="btn text-white bg-[#fa5c5c] hover:bg-[#d44]">patreon</Link>
+              <Link href={"https://ko-fi.com/zodiepupper"} className="btn text-white bg-[#fa5c5c] hover:bg-[#d44]">ko-fi</Link>
+            </div>
+            <div className="card-actions justify-end ">
+              <Link href={"https://matrix.to/#/#pupperdev:matrix.pupper.dev"} className="btn text-white bg-[#fa5c5c] hover:bg-[#d44]">matrix</Link>
+              <Link href={"https://discord.gg/krwgRUyxDf"} className="btn text-white bg-[#fa5c5c] hover:bg-[#d44]">discord</Link>
+              <Link href={"https://meow.social/@zodiepupper"} className="btn text-white bg-[#fa5c5c] hover:bg-[#d44]">mastodon</Link>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+      <div className='flex justify-evenly'>
+        {/* BARK */}
+        <div className="card w-full lg:w-2/3 h-80 bg-base-100 text-primary-content m-2">
+          <div className="card-body">
+            <img className='max-w-[4rem] max-h-16 absolute top-0 right-0 m-8 hidden md:block' src={"/doge-color-new.svg"}/>
+            <h2 className="card-title text-4xl">bark (wip)</h2>
+            <p>A safer social XR platform
+              <br/>
+              Open source, decentralized, collaborative, and fun
+            </p>
+            <div className="card-actions justify-end ">
+              <Link href={"https://github.com/zodywoolsey/bark"} className="btn text-white bg-[#fa5c5c] hover:bg-[#d44]">bark</Link>
+              <Link href={"https://github.com/zodywoolsey/ptb"} className="btn text-white bg-[#fa5c5c] hover:bg-[#d44]">bark-vr</Link>
+            </div>
+          </div>
+        </div>
+        
+      </div>
       <div className='flex justify-evenly'>
         {/* ITCH.IO */}
         <div className="card w-full lg:w-2/3 h-80 bg-base-100 text-primary-content m-2">
