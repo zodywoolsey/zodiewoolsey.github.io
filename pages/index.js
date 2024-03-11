3010009fac38353a4e43106dbc9e3cf5351ee9cf
zodiepupper <zodiepupper@zodiepuppers-MacBook-Air.local>
2024-03-11 03:43:20 -0600

    fixed contact issue with </br> instead of <br/> and resize panels

--- pages/index.js
+++ pages/index.js
@@ -5,29 +5,29 @@ export default function Home() {
     <>
     <div className='flex flex-col justify-evenly'>
       
-    <div className='flex justify-evenly'>
-        {/* BIO */}
-      <div className="card card-side card-compact w-full lg:w-2/3 h-[32rem] m-2 image-full">
-        <figure><img src="/RockyPurpleLava.jpg" alt="Squares" className='h-full'/></figure>
-        <div className="card-body">
-          <h2 className="card-title text-primary-content text-4xl">Hi, I'm Zodie</h2>
-          <div>
-            <p className='text-primary-content text-lg m-4' >I'm working on a better future for social XR applications</p>
-            <p className='text-primary-content text-lg m-4' >My current project is called "bark" and our goal is to create an open source social XR ecosystem to provide a better ground layer for future platforms</p>
-            <p className='text-primary-content text-lg m-4' >We are currently developing our client and will be working to publish a spec soon</p>
-            <p className='text-primary-content text-lg m-4 hidden xs:block' >We plan to federate with every other platform we can and offer an open set of tools and documentation</p>
+      <div className='flex justify-evenly'>
+          {/* BIO */}
+        <div className="card card-side card-compact w-full lg:w-2/3 h-[32rem] m-2 image-full">
+          <figure><img src="/RockyPurpleLava.jpg" alt="Squares" className='h-full'/></figure>
+          <div className="card-body">
+            <h2 className="card-title text-primary-content text-4xl">Hi, I'm Zodie</h2>
+            <div>
+              <p className='text-primary-content text-lg m-4' >I'm working on a better future for social XR applications</p>
+              <p className='text-primary-content text-lg m-4' >My current project is called "bark" and our goal is to create an open source social XR ecosystem to provide a better ground layer for future platforms</p>
+              <p className='text-primary-content text-lg m-4' >We are currently developing our client and will be working to publish a spec soon</p>
+              <p className='text-primary-content text-lg m-4 hidden xs:block' >We plan to federate with every other platform we can and offer an open set of tools and documentation</p>
+            </div>
           </div>
         </div>
+        
       </div>
-      
-    </div>
       <div className='flex justify-evenly'>
         {/* CONTACT */}
-        <div className="card card-side w-full lg:w-2/3  h-[28rem] bg-base-100 text-primary-content m-2">
+        <div className="card card-side w-full lg:w-2/3  h-80 bg-base-100 text-primary-content m-2">
           <div className="card-body">
             <h2 className="card-title text-4xl">Want to contract or contact us?</h2>
             <p className=''>
-              Need a website, app, or software of some other kind? Hit us up and we'll figure out how to get you what you or your business needs!</br>
+              Need a website, app, or software of some other kind? Hit us up and we'll figure out how to get you what you or your business needs!<br/>
               Our engineers have over 10 years of experience creating and designing software.
             </p>
             <div className="card-actions justify-end ">
@@ -37,11 +37,11 @@ export default function Home() {
             </div>
           </div>
         </div>
-      
-    </div>
+        
+      </div>
       <div className='flex justify-evenly'>
         {/* SUPPORT */}
-        <div className="card card-side w-full lg:w-2/3  h-[28rem] bg-base-100 text-primary-content m-2">
+        <div className="card card-side w-full lg:w-2/3  h-80 bg-base-100 text-primary-content m-2">
           <div className="card-body">
             <h2 className="card-title text-4xl">Please consider supporting us</h2>
             <p className=''>The following links are for support vectors to support the team
